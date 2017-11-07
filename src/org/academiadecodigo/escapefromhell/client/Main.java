a41c5b97960439be5a7aad596574dea82eaf2aec
Bruno Matos <brunocostamatos.24@gmail.com>
2017-11-07 13:58:45 +0000

    First Commit

---
+++ src/org/academiadecodigo/escapefromhell/client/Main.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.escapefromhell.client;
+
+/**
+ * EscapeFromHell Created by BrunoM24 on 07/11/2017.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/org/academiadecodigo/escapefromhell/server/Main.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.escapefromhell.server;
+
+/**
+ * EscapeFromHell Created by BrunoM24 on 07/11/2017.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
