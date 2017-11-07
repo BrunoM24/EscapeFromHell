85e2882dcafe2b07e27bcb20f2e2234fe85b42ca
Ricardo Hans <ricardohans@gmail.com>
2017-11-07 20:45:44 +0000

    Added some rules of movement and spawn player from top random position

--- src/org/academiadecodigo/escapefromhell/client/Grid.java
+++ src/org/academiadecodigo/escapefromhell/client/Grid.java
@@ -32,13 +32,20 @@ public class Grid {
                 grid[row][col] = false;
             }
         }
+        /*for (int i = cols; i < cols; i++) {
+            grid[29][i] = true;
+        }*/
     }
 
     public void init() {
 
         this.screen = TerminalFacade.createScreen();
         screen.startScreen();
-        screen.setCursorPosition(this.screen.getTerminalSize().getColumns() / 2 - 1, this.screen.getTerminalSize().getRows() - 1);
+        // screen.setCursorPosition(this.screen.getTerminalSize().getColumns() / 2 - 1, this.screen.getTerminalSize().getRows() - 1);
+
+        // random spawn from row 0
+
+        spawnPlayer();
 
         refresh();
 
@@ -63,7 +70,7 @@ public class Grid {
                     drawRight();
                 }
 
-                if(key.getCharacter() == 'p'){
+                if (key.getCharacter() == 'p') {
                     showGrid();
                 }
 
@@ -73,12 +80,17 @@ public class Grid {
 
     }
 
+    private void spawnPlayer () {
+
+        screen.setCursorPosition((int)(Math.random()*(this.screen.getTerminalSize().getColumns()-1)), 0);
+
+    }
     private void showGrid() {
         for (int row = 0; row < 30; row++) {
             for (int col = 0; col < 100; col++) {
-                if(grid[row][col]){
+                if (grid[row][col]) {
                     System.out.print("1");
-                }else {
+                } else {
                     System.out.print("0");
                 }
             }
@@ -97,29 +109,60 @@ public class Grid {
     }
 
     private void moveRight() {
-        //TODO check fall
-        if(grid[this.screen.getCursorPosition().getRow()][this.screen.getCursorPosition().getColumn() + 1]){
+        if (grid[this.screen.getCursorPosition().getRow()][this.screen.getCursorPosition().getColumn() + 1]) {
+
+            if(grid[this.screen.getCursorPosition().getRow()-1][this.screen.getCursorPosition().getColumn() + 1]) {
+                return;
+            }
 
             this.screen.setCursorPosition(this.screen.getCursorPosition().getColumn() + 1, this.screen.getCursorPosition().getRow() - 1);
 
-        }else {
+        } else {
 
             this.screen.setCursorPosition(this.screen.getCursorPosition().getColumn() + 1, this.screen.getCursorPosition().getRow());
         }
-
+        checkFall();
         refresh();
+        //TODO check fall
     }
 
     private void moveLeft() {
-        //TODO check fall
-        if(grid[this.screen.getCursorPosition().getRow()][this.screen.getCursorPosition().getColumn() - 1]){
+        if (grid[this.screen.getCursorPosition().getRow()][this.screen.getCursorPosition().getColumn() - 1]) {
+
+            if(grid[this.screen.getCursorPosition().getRow()-1][this.screen.getCursorPosition().getColumn() - 1]) {
+                return;
+            }
 
             this.screen.setCursorPosition(this.screen.getCursorPosition().getColumn() - 1, this.screen.getCursorPosition().getRow() - 1);
-        }else {
+        } else {
 
             this.screen.setCursorPosition(this.screen.getCursorPosition().getColumn() - 1, this.screen.getCursorPosition().getRow());
         }
+        checkFall();
         refresh();
+        System.out.println("x " + this.screen.getCursorPosition().getColumn() + " + y " + this.screen.getCursorPosition().getRow());
+        //TODO check fall
+    }
+
+
+    private void checkFall() {
+
+        if (this.screen.getCursorPosition().getRow() == 29) {
+            return;
+        }
+        if (!grid[this.screen.getCursorPosition().getRow() + 1][this.screen.getCursorPosition().getColumn()]) {
+
+            while (!grid[this.screen.getCursorPosition().getRow() + 1][this.screen.getCursorPosition().getColumn()]) {
+                System.out.println(grid[this.screen.getCursorPosition().getRow() + 1][this.screen.getCursorPosition().getColumn()]);
+
+                this.screen.setCursorPosition(this.screen.getCursorPosition().getColumn(), this.screen.getCursorPosition().getRow() + 1);
+                if (this.screen.getCursorPosition().getRow() == 29) {
+                    break;
+                }
+            }
+
+        }
+
     }
 
     private void refresh() {
